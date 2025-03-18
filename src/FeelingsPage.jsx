fd7075c0dcf8e4c4c0fc239232be96258fdb00c6
Sean Selby <seanselbyy@gmail.com>
2025-03-19 00:01:45 +1300

    Add feelings list

--- src/FeelingsPage.jsx
+++ src/FeelingsPage.jsx
@@ -7,4 +7,20 @@ export default function FeelingsPage() {
   // Hook for navigating to the RequestPage with state
   const navigate = useNavigate();
 
+  // Predefined list of feelings (unsorted initially)
+  const predefinedFeelings = [
+    "Angry",
+    "Annoyed",
+    "Confused",
+    "Frustrated",
+    "Grateful",
+    "Upset",
+    "Overwhelmed",
+    "Worried",
+    "Sad",
+    "Happy",
+    "Stressed",
+    "Curious",
+  ];
+
 }
