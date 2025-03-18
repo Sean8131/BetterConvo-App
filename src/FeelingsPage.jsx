eddd0546278c4814cd5094d41b1b1ce670282730
Sean Selby <seanselbyy@gmail.com>
2025-03-19 00:01:09 +1300

    Add useNavigate hook

--- src/FeelingsPage.jsx
+++ src/FeelingsPage.jsx
@@ -2,4 +2,9 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // Defining the FeelingsPage component
-export default function FeelingsPage() {}
+export default function FeelingsPage() {
+
+  // Hook for navigating to the RequestPage with state
+  const navigate = useNavigate();
+
+}
