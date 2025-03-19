a275d103c8af9cd888c5cf53d7b2d89481385d8e
Sean Selby <seanselbyy@gmail.com>
2025-03-19 19:53:30 +1300

    Add useNavigate hook

--- HomePage.jsx
+++ HomePage.jsx
@@ -4,5 +4,6 @@ import { useNavigate } from 'react-router-dom';
 // Define HomePage funcional component
 export default function HomePage() {
 
-    
+    // Hook to enable navigation to other routes
+    const navigate = useNavigate();
 }
