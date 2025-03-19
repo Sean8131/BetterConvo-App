ccf7e5fb9e256053adcb91234c73dfbdcb959eca
Sean Selby <seanselbyy@gmail.com>
2025-03-19 17:50:06 +1300

    Add a route for the new SituationPage component and adjust the routing order

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css"
+import SituationPage from "./SituationPage";
 import FeelingsPage from './FeelingsPage';
 import RequestPage from "./RequestPage";
 import ScriptPage from "./ScriptPage";
@@ -8,8 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Landing route can be the Feelings Page */}
-        <Route path="/" element={<FeelingsPage />} />
+        {/* Landing route: SituationPage */}
+        <Route path="/" element={<SituationPage />} />
+        <Route path="/feelings" element={<FeelingsPage />} />
         <Route path="/request" element={<RequestPage />} />
         {/* Route for the Script Response Screen */}
         <Route path="/script" element={<ScriptPage />} />
