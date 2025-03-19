f88fbddeeaf498bc41a210f8499851cf9811fd18
Sean Selby <seanselbyy@gmail.com>
2025-03-19 17:29:13 +1300

    Erase useLocation hook, declare navigate variable

--- src/SituationPage.jsx
+++ src/SituationPage.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function SituationPage() {
 
+    // Hook to enable navigation between pages
+    const navigate = useNavigate();
 }
