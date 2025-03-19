d1ea1b496a338bf51ba068ece0ca64c893966fae
Sean Selby <seanselbyy@gmail.com>
2025-03-19 20:01:07 +1300

    Add HomePage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css"
+import HomePage from "../HomePage";
 import SituationPage from "./SituationPage";
 import FeelingsPage from './FeelingsPage';
 import RequestPage from "./RequestPage";
@@ -9,8 +10,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Landing route: SituationPage */}
-        <Route path="/" element={<SituationPage />} />
+        {/* Landing route: HomePage */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/situation" element={<SituationPage />} />
         <Route path="/feelings" element={<FeelingsPage />} />
         <Route path="/request" element={<RequestPage />} />
         {/* Route for the Script Response Screen */}
