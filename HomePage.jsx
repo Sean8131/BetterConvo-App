0ea54dddd0e4db12be7c85b7e566875d837ab78f
Sean Selby <seanselbyy@gmail.com>
2025-03-19 19:58:22 +1300

    Add 'Accept' button

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 // Define HomePage funcional component
 export default function HomePage() {
+  // Hook to enable navigation to other routes
+  const navigate = useNavigate();
 
-    // Hook to enable navigation to other routes
-    const navigate = useNavigate();
+  // Handler for the "Accept" button
+  // Navigates to the SituationPage when clicked
+  const handleAccept = () => {
+    navigate("/situation");
+  };
 
-    // Handler for the "Accept" button
-    // Navigates to the SituationPage when clicked
-    const handleAccept = () => {
-        navigate('/situation');
-    };
+  return (
+    <div>
+      <h1>Welcome to BetterConvo</h1>
 
-    return(
-        <div>
-            <h1>Welcome to BetterConvo</h1>
+      <div>
+        <p>Privacy Disclaimer</p>
+      </div>
 
-            <div>
-                <p>
-                    Privacy Disclaimer
-                </p>
-            </div>
-        </div>
-    )
+      <button onClick={handleAccept}>Accept</button>
+    </div>
+  );
 }
