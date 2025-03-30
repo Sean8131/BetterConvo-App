ee13f341f0ef3b3987f5b6a310a6c9cd314f9ad2
Sean Selby <seanselbyy@gmail.com>
2025-03-30 22:55:52 +1300

    Remove focus from the buttons after they're clicked

--- src/FeelingsPage.jsx
+++ src/FeelingsPage.jsx
@@ -87,7 +87,11 @@ export default function FeelingsPage() {
             {predefinedFeelings.map((feeling) => (
               <button
                 key={feeling}
-                onClick={() => handleFeelingClick(feeling)}
+                onClick={(e) => {
+                  handleFeelingClick(feeling);
+                  // Remove focus from the button after it's clicked
+                  e.target.blur();
+                }}
                 className={`text-purple-100 hover:bg-purple-400 border-purple-200 hover:text-purple-950 rounded-lg border py-2 px-3 text-sm md:text-xl font-medium bg-[#1a1a1a] cursor-pointer transition-colors duration-200 hover:border-[#646cff] focus:outline-none focus-visible:ring-4px ${ selectedFeelings.includes(feeling) ? "bg-purple-100 text-purple-950" : "bg-[#1a1a1a] text-purple-100"
                 }`}
               >
