efb406ad2d8fde779ae4a7003da3464761a703eb
Sean Selby <seanselbyy@gmail.com>
2025-03-30 23:27:10 +1300

    Delete unnecessary code

--- src/FeelingsPage.jsx
+++ src/FeelingsPage.jsx
@@ -93,7 +93,7 @@ export default function FeelingsPage() {
             }}
             // Hide default tap highlight on WebKit-based browsers
             style={{ WebkitTapHighlightColor: "transparent" }}
-            className={`text-purple-100 border-purple-200  rounded-lg border py-2 px-3 text-sm md:text-xl font-medium bg-[#1a1a1a] cursor-pointer transition-colors duration-200 focus:outline-none focus-visible:ring-4px ${
+            className={`text-purple-100 border-purple-200  rounded-lg border py-2 px-3 text-sm md:text-xl font-medium bg-[#1a1a1a] cursor-pointer transition-colors duration-200 ${
               selectedFeelings.includes(feeling)
                 ? "bg-purple-100 text-purple-950"
                 : ""
@@ -103,17 +103,6 @@ export default function FeelingsPage() {
           </button>
         ))}
       </div>
-
-      <style>
-        {`
-          @media (hover: none) {
-            button:focus {
-              outline: none;
-              box-shadow: none;
-            }
-          }
-        `}
-      </style>
 
       {/* Navigation Buttons */}
       <div class="flex justify-center gap-2">
