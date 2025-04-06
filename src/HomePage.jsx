39cc0fa39bfd814a777dde9dde0ba1876dccb16e
Sean Selby <seanselbyy@gmail.com>
2025-04-06 22:54:30 +1200

    Add secondary button, change logo

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import MainButton from "./MainButton";
+import SecondaryButton from "./SecondaryButton"
 
 // Define HomePage funcional component
 export default function HomePage() {
@@ -15,7 +16,7 @@ export default function HomePage() {
 
   return (
     <div class="mx-auto max-width-sm md:w-xl p-2">
-      <img src="/assets/BetterConvoLogo.png" alt="BetterConvo logo" className="w-28 h-auto mx-auto mb-4"></img>
+      <img src="/assets/BetterConvoLogo.png" alt="BetterConvo logo" className="w-fit h-auto mx-auto mb-4"></img>
       <h1 class="text-3xl md:text-4xl font-bold p-4">
         Welcome!
       </h1>
@@ -24,27 +25,10 @@ export default function HomePage() {
         BetterConvo helps you prepare for challenging conversations so you can speak with clarity, empathy, and courage.
         </h2>
 
-      <p class="mb-4">
-        Follow the prompts on the next three pages. 
-      </p>
-      <p class="mb-4">The app will ask you to
-        provide:
-      </p>
 
-      <p class>
-        <div class="flex justify-center">
-          <ul class="font-medium mb-4 text-left">
-            <ol>1. A description of your situation</ol>
-            <ol>2. Your feelings about what happened</ol>
-            <ol>3. What you would like to happen next </ol>
-          </ul>
-        </div>
-      </p>
-      <p class="mb-6">
-        The app will then provide a script to help you start the conversation in a mindful way.
-      </p>
 
     <MainButton label="Next" onClick={handleAccept}/>
+    <SecondaryButton label="Skip" />
     </div>
   );
 }
