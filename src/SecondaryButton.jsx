55f29d4a5e6d6ef0b030f973be74e61dd4bf7d30
Sean Selby <seanselbyy@gmail.com>
2025-04-14 14:51:27 +1200

    Add bottom margin to skip button

--- src/SecondaryButton.jsx
+++ src/SecondaryButton.jsx
@@ -12,7 +12,7 @@ export default function SecondaryButton({ show, label, onClick }) {
                 exit={{ opacity: 0, y: -5 }}
                 transition={{ duration: 0.3 }}
                 onClick={onClick}
-                className="bg-[27, 31, 35] text-[#71B7FB] font-medium px-6 py-2 rounded-full hover:opacity-90 transition-all duration-200 w-xs"
+                className="bg-[27, 31, 35] text-[#71B7FB] font-medium px-6 py-2 rounded-full hover:opacity-90 transition-all duration-200 w-xs mb-8"
               >
                 {label}
               </motion.button>
