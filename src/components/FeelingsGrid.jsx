ceb68cbaa2de9a4902ddcf293a2baf76ef1ba574
Sean Selby <seanselbyy@gmail.com>
2025-04-21 21:29:11 +1200

    Decrease button padding

--- src/components/FeelingsGrid.jsx
+++ src/components/FeelingsGrid.jsx
@@ -1,6 +1,6 @@
 export default function FeelingsGrid({ feelings, selectedFeelings, onFeelingClick }) {
     return (
-      <div className="grid grid-cols-3 md:grid-cols-4 gap-2 max-w-sm md:max-w-lg mx-auto py-6 md:py-2">
+      <div className="grid grid-cols-3 md:grid-cols-4 gap-2 max-w-sm md:max-w-lg mx-auto">
         {feelings.map(({ label, emoji }) => (
           <button
             key={label}
