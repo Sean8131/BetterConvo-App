6532b61610c6aedb3527188ff3545b0cd66ebf50
Sean Selby <seanselbyy@gmail.com>
2025-04-21 21:16:41 +1200

    Resize emoji buttons on medium screens

--- src/components/FeelingsGrid.jsx
+++ src/components/FeelingsGrid.jsx
@@ -1,6 +1,6 @@
 export default function FeelingsGrid({ feelings, selectedFeelings, onFeelingClick }) {
     return (
-      <div className="grid grid-cols-3 gap-2 max-w-sm mx-auto my-6 md:gap-4 mb-8 md:mb-10">
+      <div className="grid grid-cols-3 md:grid-cols-4 gap-2 max-w-sm md:max-w-lg mx-auto py-6 md:py-2 mb-8 md:mb-10">
         {feelings.map(({ label, emoji }) => (
           <button
             key={label}
@@ -9,7 +9,7 @@ export default function FeelingsGrid({ feelings, selectedFeelings, onFeelingClic
               setTimeout(() => e.target.blur(), 0);
             }}
             style={{ WebkitTapHighlightColor: "transparent" }}
-            className={`flex flex-col items-center justify-center w-full h-full rounded-xl text-purple-100 border-purple-200 border py-1.5 px-2.5 text-sm md:text-xl font-medium bg-[#1a1a1a] cursor-pointer transition-colors duration-200 ${
+            className={`flex flex-col items-center justify-center w-full h-full rounded-xl text-purple-100 border-purple-200 border py-1.5 md:py-2.5  px-2.5 text-sm font-medium bg-[#1a1a1a] cursor-pointer transition-colors duration-200 ${
               selectedFeelings.includes(label) ? "bg-purple-100 text-purple-950" : ""
             }`}
           >
