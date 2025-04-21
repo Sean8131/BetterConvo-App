ae73055986f0b3117cfb382e301277a99d188d0c
Sean Selby <seanselbyy@gmail.com>
2025-04-21 21:36:19 +1200

    Add padding to emoji container

--- src/components/FeelingsGrid.jsx
+++ src/components/FeelingsGrid.jsx
@@ -1,5 +1,6 @@
 export default function FeelingsGrid({ feelings, selectedFeelings, onFeelingClick }) {
     return (
+      <div className="pb-28 pt-4">
       <div className="grid grid-cols-3 md:grid-cols-4 gap-2 max-w-sm md:max-w-lg mx-auto">
         {feelings.map(({ label, emoji }) => (
           <button
@@ -17,6 +18,7 @@ export default function FeelingsGrid({ feelings, selectedFeelings, onFeelingClic
             <span>{label}</span>
           </button>
         ))}
+      </div>
       </div>
     );
   }
